955ca90d310f184e700da08f95670ff5b4d9a9ef
Mohammed Awaiz <awaiz.web@gmail.com>
2026-02-15 22:19:32 +0530

    V6

--- app/api/contact/route.js
+++ app/api/contact/route.js
@@ -1,5 +1,5 @@
 import { NextResponse } from "next/server";
-import { connectDB } from "@/lib/connectDB";
+import { connectDB } from "@/lib/mongodb";
 import nodemailer from "nodemailer";
 
 export async function POST(req) {
